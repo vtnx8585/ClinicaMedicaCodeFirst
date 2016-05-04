3b22ff12703f73aebed42d9566a0583e902927d6
vtnx8585 <vtnx8585@gmail.com>
2016-05-04 11:45:25 -0600

    Added files via upload

---
+++ Abstracts/DataBaseOperation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ClinicaMedicaCodeFirst.Interfaces;
+using System.Data;
+
+namespace ClinicaMedicaCodeFirst.Abstracts
+{
+    public abstract class DataBaseOperation
+    {
+        public readonly IDataBaseConn databaseconn;   
+     
+        public DataBaseOperation(IDataBaseConn databaseconn) {
+            this.databaseconn = databaseconn;            
+        }
+
+        abstract public bool Create(String qry,String conn);
+        abstract public DataTable ReadForDatatable(String qry, String conn, DataTable dt);
+        abstract public int ReadForScalar(String qry, String conn, int count);
+        abstract public bool Update(String qry, String conn);
+        abstract public bool Delete(String qry, String conn);
+    }
+}
